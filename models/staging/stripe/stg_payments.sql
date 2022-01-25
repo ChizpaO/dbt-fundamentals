eb6850132064a425644eb9a10d2efa8b5d0ea45c
Luis Vega <chizpao@gmail.com>
2022-01-25 17:29:54 +0000

    jinja_macros

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,9 +1,11 @@
-SELECT  id AS payment_id,
-        orderid AS order_id,
-        paymentmethod AS payment_method,
-        status,
-        --amount is in cents, convert it to dollars
-        amount/100 AS amount,
-        created
+SELECT  
+    id AS payment_id,
+    orderid AS order_id,
+    paymentmethod AS payment_method,
+    status,
+
+    --amount is in cents, convert it to dollars
+    {{ cents_to_dollars() }} AS amount,
+    created
             
 FROM {{source('stripe','payment')}}
